4cd2b78bbf12982ae9096d538be7161798553963
Tristan Cartledge <tristan@speakeasyapi.dev>
2023-02-27 10:36:26 +0000

    fix: reset accounting sdk to regenerate

--- accounting/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/codatio/client-sdk-go/accounting
-
-go 1.14
-
-require (
-	github.com/cenkalti/backoff/v4 v4.2.0
-)
--- accounting/pkg/models/shared/security.go
+++
@@ -1,9 +0,0 @@
-package shared
-
-type SchemeAPIKey struct {
-	APIKey string `security:"name=Authorization"`
-}
-
-type Security struct {
-	APIKey SchemeAPIKey `security:"scheme,type=apiKey,subtype=header"`
-}
